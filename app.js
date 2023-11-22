66e8f488f838d44d7eeb3157ac5f0e6504b2a2ff
gene <grsiepka@gmail.com>
2023-11-21 21:36:07 -0500

    misc syntax and missing quotes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var http = require(http);
+var http = require('http');
 var timeout = 100000; //sleep 100 seconds
 
 http.createServer(function (req, res) {
@@ -10,18 +10,14 @@ http.createServer(function (req, res) {
         // Set reponse body
         res.write('Hello quick response bro');
         res.end();
-    
     }
-    else if (req.url == "/wait") {
+    else if (req.url == '/wait') {
         setTimeout((function() {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end('<html><body><p><h1> Hello this response took a while bro </h1></p></body></html>');
         }), timeout);
-
     }
     else
     res.end('Invalid Request bro!');
-    
-
 }).listen(8080);
 console.log('Node.js Wait app running on port 8080..')
